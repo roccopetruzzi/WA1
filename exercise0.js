e628d0cf02245b24c9dc37fa6eefd9abaf32d1ea
ROCCO LUIGI PETRUZZI s225624 <s225624@studenti.polito.it>
2020-03-23 22:48:09 +0100

    finished es0 and started es1

--- exercise0.js
+++ exercise0.js
@@ -1,3 +1,15 @@
 "use strict";
 
-// here goes the code
+const wordsList = ["hello", "thisIsAnArray", "  gr8 course\n", "WA1", "WA1-course", "Web Applications I", "Web Applications I "];
+//console.log(wordsList);
+
+function shortner(wordsList) {
+    for (let [i, w] of wordsList.entries()) {
+        w = w.trim();
+        const l = w.length;
+        (l < 2) ? wordsList[i] = '': wordsList[i] = w.substring(0, 2) + w.substring(l - 2);
+    }
+}
+
+shortner(wordsList);
+//console.log(wordsList);
---
+++ exercise1.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const wordsList = ["hello", "thisIsAnArray", "  gr8 course\n", "WA1", "WA1-course", "Web Applications I", "Web Applications I "];
+console.log(wordsList);
+
+for (let [i, w] of wordsList.entries()) {
+    w = w.trim();
+    const l = w.length;
+    (l < 2) ? wordsList[i] = '': wordsList[i] = w.substring(0, 2) + w.substring(l - 2);
+}
+console.log(wordsList);
+
+// // Create acronyms for the courses
+
+// const acronyms = [];
+
+// for (const c of courses) {
+//     const words = c.split(' ');
+//     let acronym = '';
+//     for (const w of words)
+//         acronym += w[0].toUpperCase();
+
+//     acronyms.push(acronym);
+// }
+
+// const output = [];
+// for (let [i, a] of acronyms.entries()) {
+//     // output.push(a + ' - ' + courses[i]) ;
+//     output.push(`${a} - ${courses[i]}`);
+// }
+
+// output.sort();
+// console.log(output);
+
+// // console.log(courselist) ;
+// console.log(courses);
+// console.log(acronyms);
+
+
+// console.log(words);
