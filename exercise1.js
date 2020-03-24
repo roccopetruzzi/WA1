330e23d5396b6a509af80eb46c6e19b16dd7c120
roccopetruzzi <roccopetruzzi1@gmail.com>
2020-03-24 23:30:02 +0100

    add task almost done and core functions

--- exercise1.js
+++ exercise1.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//  FUNCTION TO PRINT MAIN MENU
 // ver 0.0
 // function printOptions() {
 //     console.log(`Hello! Welcome to todo_manager. You can:
@@ -16,30 +17,70 @@ const printOptions = () => console.log(
     3. Show all the tasks;
     4. Close the program;`);
 
+//  FUNCTION TO GET USER INPUT
 // ver 0.0
 // function getOption() {
 //     var readlineSync = require('readline-sync');
 //     var option = readlineSync.question('> ');
 //     return option
 // }
-// ver 1.0
-// const getOption = () => {
-//     var readlineSync = require('readline-sync');
-//     return readlineSync.question('> ');
-// };
 // ver 2.0
-const getOption = (readlineSync = require('readline-sync')) => readlineSync.question('> ');
+//const getOption = (readlineSync = require('readline-sync')) => readlineSync.question('> ');
+// ver 1.0
+const getInput = (question) => {
+    var readlineSync = require('readline-sync');
+    return readlineSync.question(question);
+};
 
-printOptions();
-console.log(getOption());
+// FUNCTION TO GET Y/n ANSWER -> to modify...
+const yesOrNo = (readlineSync = require('readline-sync')) => readlineSync.question('Please anwser Y (Yes) or n (No). ').toUpperCase();
+
+function Task(desc, dead = new Date(), urg = false, pri = true) {
+    this.desc = desc;
+    this.dead = dead;
+    this.urg = urg;
+    this.pri = pri;
+}
+
+//  MAIN
+const appointments = [];
+let option = '';
+while (option != 4) {
+    printOptions();
+    option = getInput('> ');
+    switch (option) {
+        case "1":
+            let desc = getInput("Please, provide the task description: ");
+
+            let urg = getInput("Is it urgent? [Y/n] ").toUpperCase();
+            while (urg.length > 1 || (urg != 'N' && urg != 'Y')) {
+                urg = yesOrNo();
+            }
+            (urg == 'Y') ? urg = true: urg = false;
+            let pri = getInput("Is it private? [Y/n] ");
+            while (pri.length > 1 || (pri != 'N' && pri != 'Y')) {
+                pri = yesOrNo();
+            }
+
+            let dead = getInput("When do you want to schedule it? ");
+            appointments.push(new Task(desc, dead, urg, pri));
+            break;
+        case "2":
+            console.log("opt2");
+            break;
+        case "3":
+            console.log("opt3");
+            break;
+        case "4":
+            return 0;
+            break;
+        default:
+            console.log("Sorry, you entered an invalid option. Please try again.");
+            break;
+    }
+}
 
 //HELPER 
-// function Task(description, deadline = new Date(), urgent = false, private = true) {
-//     this.description = description;
-//     this.deadline = deadline;
-//     this.urgent = urgent;
-//     this.private = private;
-// }
 
 // const wordsList = ["hello", "thisIsAnArray", "  gr8 course\n", "WA1", "WA1-course", "Web Applications I", "Web Applications I "];
 // console.log(wordsList);
