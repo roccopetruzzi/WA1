74675b9bd2bdc4a45d6039280e5dfb78af1d3047
roccopetruzzi <roccopetruzzi1@gmail.com>
2020-03-28 12:12:22 +0100

    exercise1-3 completed. objects and functions/methods all working; to implement: Timer(ex4).

--- exercise1.js
+++ exercise1.js
@@ -1,21 +1,6 @@
 "use strict";
 
-//  FUNCTION TO PRINT MAIN MENU
-// ver 0.0
-// function printOptions() {
-//     console.log(`Hello! Welcome to todo_manager. You can:
-//     1. Insert a new task;
-//     2. Remove a task;
-//     3. Show all the tasks;
-//     4. Close the program;`);
-// }
-// ver 1.0
-const printOptions = () => console.log(
-    `Hello! Welcome to todo_manager. You can:
-    1. Insert a new task;
-    2. Remove a task;
-    3. Show all the tasks;
-    4. Close the program;`);
+/********************* FUNCTIONS *********************/
 
 //  FUNCTION TO GET USER INPUT
 // ver 0.0
@@ -47,6 +32,9 @@ const yesOrNo = (question) => {
     return ans;
 };
 
+/********************* OBJECTS *********************/
+
+// TASK CONSTRUCTOR
 function Task(desc, dead = new Date(), urg = false, pri = true) {
     this.description = desc;
     this.deadline = dead;
@@ -54,6 +42,21 @@ function Task(desc, dead = new Date(), urg = false, pri = true) {
     this.isPrivate = pri;
 }
 
+var Menu = {
+    option: '',
+    printOptions: function() {
+        console.log(`Hello! Welcome to todo_manager. You can:
+    1. Insert a new task;
+    2. Remove a task;
+    3. Show all the tasks;
+    4. Close the program;`);
+    },
+    askForInput: function() {
+        this.option = getInput('> ');
+    }
+}
+
+// CALENDAR PROTOTYPE
 var Calendar = {
     name: '',
     appointments: new Map(),
@@ -96,22 +99,23 @@ var Calendar = {
     }
 }
 
-//  MAIN
-let option = '';
+/********************* MAIN *********************/
+
 var cal = Object.create(Calendar);
-cal.name = "Rocco";
-while (option != 4) {
-    printOptions();
-    option = getInput('> ');
-    switch (option) {
-        case "1":
+var men = Object.create(Menu);
+cal.name = "Personal";
+while (men.option != 4) {
+    men.printOptions();
+    men.askForInput();
+    switch (Number(men.option)) {
+        case 1:
             let desc = getInput("Please, provide the task description: "); //.toUpperCase();
             let urg = yesOrNo("Is it urgent? [Y/n] ");
             let pri = yesOrNo("Is it private? [Y/n] ");
             let dead = new Date(Date.parse(getInput("When do you want to schedule it? ")));
             cal.addTask(new Task(desc, dead, urg, pri));
             break;
-        case "2":
+        case 2:
             let ans = getInput("Do you want to delete it by Deadline (A) or Description (B)? ").toUpperCase();
             switch (ans) {
                 case "A":
@@ -127,10 +131,10 @@ while (option != 4) {
                     break;
             }
             break;
-        case "3":
+        case 3:
             cal.printAllTask();
             break;
-        case "4":
+        case 4:
             console.log("Goodbye...!");
             return 0;
             break;
@@ -139,3 +143,17 @@ while (option != 4) {
             break;
     }
 }
+
+/*
+TESTING AREA...
+    setInterval(this.askForInput, 1000);
+    setOption: function(val) {
+        this.option = val;
+    },
+    hasValidOption: function() {
+        (this.option <= this.possibleOptions && this.option > 0) ? true: false;
+    },
+    const sayHello = () => { console.log("Hello!"); }
+
+    setTimeout(sayHello, 1000);
+    */
